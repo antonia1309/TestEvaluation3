4331284047d7df0df674ff0da510d00c9993c4b1
Antonia <familleravel@hotmail.fr>
2023-10-06 22:07:31 +0200

    added netlify cms

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,8 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/assets");
   eleventyConfig.addPassthroughCopy("./src/style.css");
   eleventyConfig.addPassthroughCopy("./src/script.js");
+  eleventyConfig.addPassthroughCopy("./src/admin");
+
 
 
   return{
